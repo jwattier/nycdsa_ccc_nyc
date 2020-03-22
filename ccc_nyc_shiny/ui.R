571f23a67094bd27694c9b55588254c510d9a5c2
Jason Wattier <jasonwattier@gmail.com>
2020-03-22 09:28:38 -0500

    changed UI layout to Shiny dashboard

--- ccc_nyc_shiny/ui.R
+++ ccc_nyc_shiny/ui.R
@@ -9,28 +9,30 @@
 
 library(shiny)
 
+
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("NYC Access Map"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
+shinyUI(dashboardPage(
+    dashboardHeader(title = "CCC NYC Analysis"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem(text = "Map", tabName = "map", icon = icon("map")),
+            menuItem(text = "Explore", tabName = "explore", icon = icon("chart-area")),
             selectizeInput(inputId="select_category",
-                        label="Resource Category:",
-                        choices = resouse_categories)
+                           label="Resource Category:",
+                           choices = resouse_categories)
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            h4("Heatmap of Accessiblity"),
-            leafletOutput("accessMap"),
-            
-            # Output is a display of the allocations percentages should that option be selected
-            h4("Category Percentages"),
-            DT::dataTableOutput("perc_table")
+    dashboardBody(
+        tabItem(tabName = "map",
+                    h4("Heatmap of Accessiblity"),
+                    leafletOutput("accessMap"),
+                    
+                    # Output is a display of the allocations percentages should that option be selected
+                    h4("Category Percentages"),
+                    DT::dataTableOutput("perc_table")
+                ),
+        tabItem(tabName = "explore",
+                )
         )
     )
-))
+)
